27271314c50ae5d1c73ca0f0632ab5abf5f02d1a
anesitietie-rgb <anesi.tietie@my.rongotai.school.nz>
2026-08-11 21:01:06 +0000

    update newfiles

---
+++ include/footer.php
@@ -0,0 +1,18 @@
+
+  <div class="card text-center">
+  <div class="card-header">
+    Featured
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+  <div class="card-footer text-body-secondary">
+    2 days ago
+  </div>
+</div>
+  
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$page_title = "home";
+include("include/header.php");
+include("include/nav.php");
+include("include/carousel.php");
+
+?>
+<h1>Hello, world!</h1>
+
+
+
+
+
+<?php
+include("include/footer.php");
+
+?>
