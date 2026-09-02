1764ec0073a170356ae62caa9ebf023c6f9ee92e
anesitietie-rgb <anesi.tietie@my.rongotai.school.nz>
2026-09-02 14:12:24 +1200

    Update control_panel.php

--- control_panel.php
+++ control_panel.php
@@ -18,8 +18,8 @@ include('includes/nav.php');
 <!-- Start of content one -->
 <div class ="container">
     <div class="row">
-        <div class="col-sm-3">
- <div class="col-sm-6 text-center">
+        <div class="col-sm-3"></div>
+        <div class="col-sm-6 text-center">
             <h2 class="pb-4">Welcome back, <?= htmlspecialchars($_SESSION['firstname'] ?? 'Manager'); ?></h2>
             <a href="borrow.php"><button class="btn btn-danger btn-lg m-2">Log a Loan</button></a>
             <a href="manage_loans.php"><button class="btn btn-primary btn-lg m-2">Manage Loans</button></a>
