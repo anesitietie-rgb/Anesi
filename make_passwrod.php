d68c192523412bd19cc2697db265afe14b7def83
anesitietie-rgb <anesi.tietie@my.rongotai.school.nz>
2026-09-06 16:18:47 +1200

    Delete make_passwrod.php

--- make_passwrod.php
+++
@@ -1,9 +0,0 @@
-<?php
-$password = "password67";
-$hash = password_hash($password, PASSWORD_DEFAULT);
-if (password_verify(password, $hash)) {
-  echo "Password is correct!";
-}else {
-  echo "Password is incorrect!;
-}
-?>
