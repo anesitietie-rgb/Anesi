7df1117c85f601919acf8ff80144cfeecdedfee6
anesitietie-rgb <anesi.tietie@my.rongotai.school.nz>
2026-09-06 16:28:49 +1200

    Correct password verification logic
    Fix password verification by using the correct variable.

--- make_password.php
+++ make_password.php
@@ -1,1 +1,9 @@
-
+<?php
+$password = "password67";
+$hash = password_hash($password, PASSWORD_DEFAULT);
+if (password_verify(password, $hash)) {
+  echo "Password is correct!";
+}else {
+  echo "Password is incorrect!;
+}
+?>
